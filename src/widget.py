a10bbc3b4605addf63be145f0eaa23510d42a09e
Kirill Lukash <kirill.lukash.91@mail.ru>
2024-07-03 16:35:25 +0700

    masks.py

---
+++ src/widget.py
@@ -0,0 +1,15 @@
+from src.masks import mask_card, mask_account
+
+def mask_account_card(input_string: str) -> str | None:
+    """Функция маскировки карты или счета. """
+    if "Visa Platinum" in input_string or "Maestro" in input_string:
+        return mask_account_card(input_string)
+    elif "Счeт" in input_string:
+        return mask_account_card(input_string)
+
+
+def get_data(input_string: str) -> str | None:
+    """Функция преобразования даты. """
+    date = input_string.split("T")[0]
+    format_date = f"{date[-2:]}.{date[5:7]}.{date[:4]}"
+    return format_date
