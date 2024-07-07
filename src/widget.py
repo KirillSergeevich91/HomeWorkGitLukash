8a5fd19393c7b67a7d0ff9e78294c6b30a59a506
Kirill Lukash <kirill.lukash.91@mail.ru>
2024-07-07 23:26:51 +0700

    commit

--- src/widget.py
+++ src/widget.py
@@ -8,7 +8,7 @@ def mask_account_card(input_string: str) -> str | None:
         return mask_account_card(input_string)
 
 
-def get_data(input_string: str) -> str | None:
+def get_date(input_string: str) -> str | None:
     """Функция преобразования даты. """
     date = input_string.split("T")[0]
     format_date = f"{date[-2:]}.{date[5:7]}.{date[:4]}"
